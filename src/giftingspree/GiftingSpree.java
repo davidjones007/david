827b6af20f98ce7456f06115652019b8920a2d2a
kooshar <koosha_rzd@yahoo.com>
2015-11-25 19:10:35 -0800

    PPT

--- src/giftingspree/GiftingSpree.java
+++ src/giftingspree/GiftingSpree.java
@@ -1,23 +1,41 @@
 package giftingspree;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GiftingSpree {
+    /**
+     * Given a budget and a list of item prices, determine the maximum number of
+     * items that can be purchased. Prices that are smaller than or equal to 0
+     * represent invalid items and should be ignored.
+     * 
+     * @param budget
+     *            >= 0
+     * @param priceList
+     *            where entries less than or equal to 0 should be igmored
+     * @return the maximum number of items that can be purchased
+     */
+    public static int getMaximumItems(double budget, List<Double> priceList) {
+        // making a positive sorted list
+        List<Double> positiveSortedList = new ArrayList<Double>();
+        for (int i = 0; i < priceList.size(); i++) {
+            if (priceList.get(i) > 0) {
+                positiveSortedList.add(priceList.get(i));
+            }
+        }
+        Collections.sort(positiveSortedList);
 
-	/**
-	 * Given a budget and a list of item prices, determine the maximum number of
-	 * items that can be purchased. Prices that are smaller than or equal to 0
-	 * represent invalid items and should be ignored.
-	 * 
-	 * @param budget
-	 *            >= 0
-	 * @param priceList
-	 *            where entries less than or equal to 0 should be igmored
-	 * @return the maximum number of items that can be purchased
-	 */
-	public static int getMaximumItems(double budget, List<Double> priceList) {
-		// TODO: Implement this method
-		return -1;
-	}
+        int i = 0;
+        int numOfGifts = 0;
+        while (budget > 0 && i < positiveSortedList.size()) {
+            if (positiveSortedList.get(i) <= budget) {
+                budget -= positiveSortedList.get(i);
+                numOfGifts++;
+            }
+            i++;
+        }
 
+        return numOfGifts;
+    }
 }
--- src/packingcubes/PackingCubes.java
+++ src/packingcubes/PackingCubes.java
@@ -2,18 +2,35 @@ package packingcubes;
 
 public class PackingCubes {
 
-	/**
-	 * Return the minimum cost of a cuboid box needed to pack N 1x1x1 cubes. The
-	 * cost of a box of dimensions d1xd2xd3 is d1+d2+d3.
-	 * 
-	 * @param N
-	 *            the number of 1x1x1 cubes that need to be packed
-	 * @return the cost of the cheapest box that can hold all N cubes without
-	 *         any additional space
-	 */
+    /**
+     * Return the minimum cost of a cuboid box needed to pack N 1x1x1 cubes. The
+     * cost of a box of dimensions d1xd2xd3 is d1+d2+d3.
+     * 
+     * @param N
+     *            the number of 1x1x1 cubes that need to be packed
+     * @return the cost of the cheapest box that can hold all N cubes without
+     *         any additional space
+     */
 
-	public static int getMinimumCost(int N) {
-		// TODO: Implement this method
-		return -1;
-	}
+    public static int getMinimumCost(int N) {
+        //start with the case of a box of N*1*1
+        int lowestCost = N + 1 + 1;
+
+        //try different whole numbers for dimension and correct the lowestCost 
+        for (int length = 1; length <= N; length++) {
+            if (N % length == 0) {
+                int dimension = N / length;
+
+                for (int width = 1; width <= dimension; width++) {
+                    if (dimension % width == 0) {
+                        int height = dimension / width;
+
+                        int cost = height + width + length;
+                        lowestCost = Math.min(lowestCost, cost);
+                    }
+                }
+            }
+        }
+        return lowestCost;
+    }
 }
